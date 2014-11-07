1e395d7fd0ec0a4b544e2293a01a77497fe02583
Sahil Trikha <trikha6@gmail.com>
2014-11-07 11:29:11 -0500

    add angellist call

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,6 +1,7 @@
 class StartupsController < ApplicationController
   def index
     # @startups = Startup.all
+    @angel_response = HTTParty.get( https://api.angel.co/1/feed)
     render(:index)
   end
 end 
