2be0f014bae017c8059f550ab56b69075215f4bb
Sahil Trikha <trikha6@gmail.com>
2014-12-09 23:02:24 -0500

    create startup model

---
+++ db/migrate/20141210034601_create_startups.rb
@@ -0,0 +1,13 @@
+class CreateStartups < ActiveRecord::Migration
+  def change
+    create_table :startups do |t|
+      t.string :name
+      t.string :crunchbase_id
+      t.string :crunchbase_path
+      t.integer :added_to_crunchbase_at
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
